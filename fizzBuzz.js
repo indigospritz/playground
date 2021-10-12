ab6ca75afa5fe52a87a56a1e88f3a763d07c5588
christian <delmsnoob@gmail.com>
2021-10-12 18:39:54 +0800

    fizBuzz

---
+++ fizzBuzz.js
@@ -0,0 +1,13 @@
+const fizzBuzz = num => {
+    if (num % 3 == 0 && num % 5 == 0) {
+        console.log('fizzbuzz')
+    } else if (num % 3 == 0) {
+        console.log('fizz')
+    } else if (num % 5 == 0 ){
+        console.log('buzz')
+    } else {
+        console.log(undefined)
+    }
+}
+
+fizzBuzz(30)
