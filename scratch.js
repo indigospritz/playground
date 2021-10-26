fa6bb699e55981d6e052750506638c52b3c5f04f
christian <delmsnoob@gmail.com>
2021-10-26 18:59:48 +0800

    higher order functions

--- scratch.js
+++ scratch.js
@@ -113,13 +113,53 @@
 // getSum()
 // console.log(pandigitalNum)
 
-function Person (name,age, gender, runner) {
-    this.name = name
-    this.age = age
-    this.gender = gender
-    this.runner = runner
-}
+// function Person (name,age, gender, runner) {
+//     this.name = name
+//     this.age = age
+//     this.gender = gender
+//     this.runner = runner
+// }
 
-const stud1 = new Person('todd', 31, 'male', true)
-const stud2 = new Person('carly', 21, 'female', false)
-console.log(stud1, stud2);
+// const stud1 = new Person('todd', 31, 'male', true)
+// const stud2 = new Person('carly', 21, 'female', false)
+// console.log(stud1, stud2);
+
+// const myArray = [1, 2, 3, 4, 5]
+// const mySet = new Set(myArray)
+
+
+// console.log(mySet.size);
+
+// sort
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+const sortAges = ages.sort((a, b) => a - b)
+
+// reduce brute force
+
+// let ageSum = 0
+// for (let i = 0; i < ages.length; i++) {
+//     ageSum += ages[i]
+// }
+
+// reduce
+const ageSum = ages.reduce((total, age) => total + age, 0)
+
+console.log(ageSum)
+
+const companies = [
+    {name: "Company One", category: "Finance", start: 1981, end: 2003},
+    {name: "Company Two", category: "Retail", start: 1992, end: 2008},
+    {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+    {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+    {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+    {name: "Company Six", category: "Finance", start: 1987, end: 2010},
+    {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+    {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+    {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
+  ]
+
+const totalYears = companies.reduce(function(total, company){
+    return total + (company.end - company.start)
+}, 0)
+console.log(totalYears);
