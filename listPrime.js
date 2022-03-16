a377818f771b14202081e44c4b4ff7d2d8b2eb84
delmsnoob <76507763+delmsnoob@users.noreply.github.com>
2022-03-16 10:52:11 +0800

    Update listPrime.js

--- listPrime.js
+++ listPrime.js
@@ -16,3 +16,26 @@ const listPrime = range => {
   
 }
 listPrime(200)
+
+// list of all prime number within N range
+const listPrime = num => {
+  // 
+  let sum = 0
+  for (let i = 2; i < num; i++) {
+    let flag = true
+    for (let j = 2; j<=i-1; j++) {
+      if (i % j === 0) {
+        flag = false
+        break
+      }
+    }
+    if (flag) {
+      console.log(i) 
+      // return sum
+      sum+=i
+      // console.log(sum+=i)
+    }
+  }
+  return sum
+}
+console.log(listPrime(20))
