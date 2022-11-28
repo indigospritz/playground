00481a50d5a70491e81b51b7e733dd365ba7b9b8
Christian Del Monte <76507763+indigospritz@users.noreply.github.com>
2022-11-28 21:38:44 +0800

    Update numberPalindrome.js

--- js_notes/numberPalindrome.js
+++ js_notes/numberPalindrome.js
@@ -1,8 +1,9 @@
 const isPalindrome = num => {
     // convert the number into iteratable array
     const numArr = Array.from(String(num), Number)
-    // loop through the half point of the array and compare the starting index to the ending index until middle
+    // loop through the half point of the array 
     for (let i = 0; i < numArr.length - 1 / 2; i++) {
+    // compare the current index to the ending index until middle
         if (numArr[i] !== numArr[numArr.length -1 -i]) {
             return false
         }
